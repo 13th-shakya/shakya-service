0e5a97235f0807a5a027765510ca18423e660f32
Riley Ho <riley@rileychh.dev>
2024-11-22 14:58:14 +0800

    feat: initial commit

---
+++ app/src/main/java/com/example/shakyaservice/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.shakyaservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.shakyaservice.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.button.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, MyService.class);
+            startService(intent);
+            Toast.makeText(MainActivity.this, "啟動 Service", Toast.LENGTH_SHORT).show();
+            finish();
+        });
+    }
+}
